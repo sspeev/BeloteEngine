e3fc921a44bce57d8b78b528dc976b6f817f1954
sspeev <hidden.sloth16@gmail.com>
2025-03-03 07:27:07 +0200

    Moving changes

--- BeloteEngine.Web/BeloteEngine.Web/wwwroot/app.js
+++ BeloteEngine.Web/BeloteEngine.Web/wwwroot/app.js
@@ -1,3 +1,15 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
-    AOS.init();
+﻿// wwwroot/app.js
+document.addEventListener('DOMContentLoaded', function () {
+    const elements = document.querySelectorAll('.fadeinup');
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+            }
+        });
+    });
+
+    elements.forEach(element => {
+        observer.observe(element);
+    });
 });
