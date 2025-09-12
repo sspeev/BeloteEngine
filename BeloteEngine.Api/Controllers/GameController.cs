43efe881f2ee5f77016e816cad392809465b7983
Stoyan Peev <114337044+sspeev@users.noreply.github.com>
2025-09-12 14:21:36 +0300

    Fixing endpoint StartGame

--- BeloteEngine.Api/Controllers/GameController.cs
+++ BeloteEngine.Api/Controllers/GameController.cs
@@ -1,10 +1,9 @@
 ﻿using BeloteEngine.Api.Hubs;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BeloteEngine.Api.Controllers
 {
-    [Route("api/game")]
+    [Route("api/{lobbyId}")]
     [ApiController]
     public class GameController(
         ILogger<GameController> _logger
@@ -14,8 +13,8 @@ namespace BeloteEngine.Api.Controllers
         private readonly ILogger<GameController> logger = _logger;
         private readonly BeloteHub hub = _hub;
 
-        [HttpGet]
-        public async Task<IActionResult> StartGame()
+        [HttpGet($"start")]
+        public async Task<IActionResult> StartGame(int lobbyId)
         {
             await hub.OnConnectedAsync();
             return Ok("Game logic will be implemented here.");
